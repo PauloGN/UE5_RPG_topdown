72b0444c9dd762cff2e87b0af431baee2f3c0494
PauloGN <paulorsantos.gnt@outlook.com>
2024-01-22 20:43:16 -0800

    10 Setting up player controller - enhanced input modules added

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -1,5 +1,31 @@
 // Copyright Paulo R Santos. - Stephen U
 
-
 #include "Player/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
 
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+
+
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(auraContext);
+
+	UEnhancedInputLocalPlayerSubsystem* subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(subsystem);
+
+	subsystem->AddMappingContext(auraContext, 0);
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI inputModeData;
+
+	inputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	inputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(inputModeData);
+}
